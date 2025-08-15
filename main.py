8a56f0655e269640b9c21ef463312216a70570b8
CPMRm <rmcpmstore@gmail.com>
2025-08-15 23:10:17 +0800

    Update and rename main (5).py to main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from kinglancpm import CPMRm
-
-__CHANNEL_USERNAME__ = "KingLanCPM"
-__GROUP_USERNAME__   = "KingLanCPMchat"
+from funny import Funny
+__CHANNEL_USERNAME__ = "YT CAT × RMSTUDIO"
+__GROUP_USERNAME__   = "Car parking 外掛討論群 & CPM 工具 聊天室"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -39,7 +38,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name =  "Car Parking Multiplayer 1 Tool - t.me/kinglancpm"
+    brand_name =  "Car Parking Multiplayer 1 Tool - https://t.me/+iB8B6GQx0k4yNDk1"
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
@@ -134,7 +133,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMRm(acc_access_key)
+        cpm = Funny(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
